acdded9a00efc7ffaba478e7792f8a62f455697c
andersonsilva95 <anderson_ps12@hotmail.com>
2018-12-23 19:26:33 -0200

    ainda não sei como printar o type

---
+++ idade.py
@@ -0,0 +1,14 @@
+minha_idade = 23
+minha_idade
+
+
+print('Eu tenho',minha_idade, 'anos')
+
+taxaUSD = 3.20
+print (300 / taxaUSD)
+
+meu_nome = 'Anderson'
+print ('E meu nome é',meu_nome)
+
+
+type(taxaUSD)
